701873b1c1bbee69e3f2c9ad63b5cea177f8537e
FerminT <fermintravi@gmail.com>
2024-02-04 09:28:23 -0500

    Rename loss methods

--- models/loss.py
+++ models/loss.py
@@ -2,11 +2,11 @@ from torch import sum
 import torch.nn as nn
 
 
-def vae_loss(recon_x, x, mu, logvar):
-    return reconstruction_loss(recon_x, x) + kl_divergence(mu, logvar)
+def bce_kld(recon_x, x, mu, logvar):
+    return bce(recon_x, x), kl_divergence(mu, logvar)
 
 
-def reconstruction_loss(recon_x, x):
+def bce(recon_x, x):
     return nn.functional.binary_cross_entropy(recon_x, x, reduction='sum')
 
 
