c303528a79b03747a1dcae1c9e83318f159620d5
FerminT <fermintravi@gmail.com>
2024-02-05 15:17:03 -0500

    Change reconstruction loss

--- models/losses.py
+++ models/losses.py
@@ -2,12 +2,12 @@ from torch import sum
 import torch.nn as nn
 
 
-def bce_kld(recon_x, x, mu, logvar):
-    return bce(recon_x, x), kl_divergence(mu, logvar)
+def mse_kld(recon_x, x, mu, logvar):
+    return mse(recon_x, x), kl_divergence(mu, logvar)
 
 
-def bce(recon_x, x):
-    return nn.functional.binary_cross_entropy(recon_x, x, reduction='sum')
+def mse(recon_x, x):
+    return nn.functional.mse_loss(recon_x, x, reduction='sum')
 
 
 def kl_divergence(mu, logvar):
