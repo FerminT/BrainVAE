e5c64b0dfea49eef02b24d7214107ff01115ee5b
FerminT <fermintravi@gmail.com>
2024-02-10 12:33:44 -0500

    Pairwise gaussian KL function in losses.py

--- models/losses.py
+++ models/losses.py
@@ -1,4 +1,4 @@
-from torch import sum
+from torch import sum, matmul, unsqueeze
 import torch.nn as nn
 
 
@@ -12,3 +12,20 @@ def mse(recon_x, x):
 
 def kl_divergence(mu, logvar):
     return -0.5 * sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+
+def pairwise_gaussian_kl(mu, log_sigma_sq, latent_dim):
+    """ Pairwise Gaussian KL divergence, from Moyer et al. 2018
+        Used as an approximation of KL[(q(z|x) || q(z))] """
+    sigma_sq = log_sigma_sq.exp()
+    sigma_sq_inv = 1.0 / sigma_sq
+    first_term = matmul(sigma_sq, sigma_sq_inv.transpose(0, 1))
+
+    r = matmul(mu * mu, sigma_sq_inv.squeeze().transpose(0, 1))
+    r2 = (mu * mu * sigma_sq_inv.squeeze()).sum(axis=1)
+    second_term = 2 * matmul(mu, (mu * sigma_sq_inv.squeeze()).transpose(0, 1))
+    second_term = r - second_term + r2
+    det_sigma = log_sigma_sq.sum(axis=1)
+    third_term = unsqueeze(det_sigma, dim=1) - unsqueeze(det_sigma, dim=1).transpose(0, 1)
+
+    return 0.5 * (first_term + second_term + third_term - latent_dim)
