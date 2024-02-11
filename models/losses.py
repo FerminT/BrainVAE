ec0f4f55f2e42355d5fd407ae925679db6cd17f9
FerminT <fermintravi@gmail.com>
2024-02-11 12:16:49 -0500

    Refacotr kl_divergence in losses.py

--- models/losses.py
+++ models/losses.py
@@ -1,9 +1,9 @@
-from torch import sum, matmul, unsqueeze
+from torch import matmul, unsqueeze
 import torch.nn as nn
 
 
 def mse_kld(recon_x, x, mu, logvar):
-    return mse(recon_x, x), kl_divergence(mu, logvar)
+    return mse(recon_x, x), -kl_divergence(mu, logvar)
 
 
 def mse(recon_x, x):
@@ -11,7 +11,7 @@ def mse(recon_x, x):
 
 
 def kl_divergence(mu, logvar):
-    return -0.5 * sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return 0.5 * (1 + logvar - mu.pow(2) - logvar.exp()).sum(axis=1)
 
 
 def pairwise_gaussian_kl(mu, log_sigma_sq, latent_dim):
