c7d1dd40ca23100c23d9595ce2b45154b9dde2c5
FerminT <fermintravi@gmail.com>
2024-03-02 15:58:18 -0500

    Do not pin memory in dataloader

--- scripts/data_handler.py
+++ scripts/data_handler.py
@@ -12,7 +12,7 @@ from scripts import constants
 
 def get_loader(dataset, batch_size, shuffle, num_workers):
     num_workers = min(cpu_count(), num_workers)
-    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=True)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
 
 def load_metadata(datapath):
