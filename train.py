d423879bb79f1081fbc2f119e6482351de9c82a7
FerminT <fermintravi@gmail.com>
2024-07-05 10:40:11 -0300

    Add one more digit to checkpoint name

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ def train(config, train_data, val_data, batch_size, epochs, precision, log_inter
     val_loader = get_loader(val_data, batch_size, shuffle=False, num_workers=workers)
     model = ICVAE(**config)
     wandb_logger = WandbLogger(name=f'{save_path.parent.name}_{save_path.name}', project='BrainVAE', offline=no_sync)
-    checkpoint = ModelCheckpoint(dirpath=save_path, filename='{epoch:03d}-{val_recon_loss:.2f}',
+    checkpoint = ModelCheckpoint(dirpath=save_path, filename='{epoch:03d}-{val_recon_loss:.3f}',
                                  monitor='val_recon_loss', mode='min', save_top_k=5, save_last=True)
     lr_monitor = LearningRateMonitor(logging_interval='step')
     reconstruction = LogReconstructionsCallback(sample_size=8)
