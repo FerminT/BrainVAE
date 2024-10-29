c58c53b0eaefe7bfd70ada2f7f8ba1562d38302c
Fermin Travi <fermintravi@gmail.com>
2024-10-29 17:55:58 -0300

    Coloring with optional label in manifold learning plot

--- test.py
+++ test.py
@@ -1,26 +1,32 @@
 from pathlib import Path
-from scripts.constants import DATA_PATH, CFG_PATH, CHECKPOINT_PATH, EVALUATION_PATH
-from scripts.data_handler import (get_loader, get_datapath, load_set, upsample_datasets, create_test_splits,
-                                  get_balanced_indices, target_mapping, balance_dataset)
+from scripts.constants import CFG_PATH, CHECKPOINT_PATH, EVALUATION_PATH
+from scripts.data_handler import (get_loader, get_datapath, load_set, create_test_splits, target_mapping,
+                                  balance_dataset)
 from scripts.embedding_dataset import EmbeddingDataset
-from scripts.t1_dataset import T1Dataset, transform
+from scripts.t1_dataset import T1Dataset
 from scripts.utils import load_yaml, reconstruction_comparison_grid, init_embedding, subjects_embeddings, load_model
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch import Trainer, seed_everything
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, mean_absolute_error
 from models.embedding_classifier import EmbeddingClassifier
 from models.utils import get_latent_representation
 from scipy.stats import pearsonr
 from tqdm import tqdm
 from numpy import array, random
-from pandas import concat, cut, DataFrame
-from seaborn import scatterplot, kdeplot
+from pandas import DataFrame, cut
+from seaborn import scatterplot, kdeplot, set_theme, color_palette
 from PIL import ImageDraw, ImageFont
 import matplotlib.pyplot as plt
 import torch
 import wandb
 import argparse
+
+
+def save_predictions(df, predictions, labels, target_name, model_name):
+    df['predicted'] = predictions
+    df['label'] = labels
+    df = df.drop(columns=['embedding'])
+    df.to_csv(f'predictions_{model_name}_{target_name}.csv')
 
 
 def predict_from_embeddings(embeddings_df, cfg, dataset, ukbb_size, val_size, latent_dim, age_range, bmi_range, label,
@@ -44,6 +50,7 @@ def predict_from_embeddings(embeddings_df, cfg, dataset, ukbb_size, val_size, la
     mean_df = results_df.mean(axis=0).to_frame(name='Mean')
     mean_df['Std'] = results_df.std(axis=0)
     print(mean_df)
+    save_predictions(val, all_preds[0], all_labels[0], label, cfg)
 
 
 def train_classifier(train_data, val_data, config_name, latent_dim, output_dim, bin_centers, batch_size, epochs,
@@ -112,28 +119,47 @@ def sample(model, dataset, age, subject_id, device, save_path):
     print(f'Reconstructed MRI saved at {save_path / comparison_img_name}')
 
 
-def plot_embeddings(subjects_df, method, label, save_path, annotate_ids=False):
+def plot_embeddings(subjects_df, method, label, save_path, color_by=None, annotate_ids=False):
     if label not in subjects_df:
         raise ValueError(f'{label} not found in the dataframe')
     seed_everything(42, workers=True)
     save_path.mkdir(parents=True, exist_ok=True)
-    if label == 'age_at_scan' or label == 'bmi':
-        subjects_df[label] = cut(subjects_df[label], bins=3)
-        # remove middle category
-        subjects_df = subjects_df[subjects_df[label] != subjects_df[label].cat.categories[1]]
     components = init_embedding(method).fit_transform(array(subjects_df['embedding'].to_list()))
     subjects_df['emb_x'], subjects_df['emb_y'] = components[:, 0], components[:, 1]
-    fig, ax = plt.subplots()
-    # usar hexbin
-    scatterplot(data=subjects_df, x='emb_x', y='emb_y', hue=label, ax=ax, alpha=0.3, size=.3)
-    kdeplot(data=subjects_df, x='emb_x', y='emb_y', hue=label, fill=False, ax=ax)
+    set_theme()
+    fig, ax = plt.subplots(figsize=(10, 8))
+    if color_by:
+        color_by_is_float = subjects_df[color_by].dtype == 'float64'
+        if color_by_is_float:
+            subjects_df[color_by] = subjects_df[color_by].astype(int)
+        scatter = scatterplot(data=subjects_df, x='emb_x', y='emb_y', hue=color_by, style=label, ax=ax, alpha=0.5,
+                              size=.3, palette='viridis_r')
+        handles_scatter, labels_scatter = scatter.get_legend_handles_labels()
+        kdeplot(data=subjects_df, x='emb_x', y='emb_y', hue=label, fill=False, ax=ax, alpha=0.8)
+        if color_by_is_float:
+            norm = plt.Normalize(subjects_df[color_by].min(), subjects_df[color_by].max())
+            sm = plt.cm.ScalarMappable(cmap='viridis_r', norm=norm)
+            sm.set_array([])
+            cbar = plt.colorbar(sm, ax=ax)
+            cbar.set_label(color_by)
+        handles_kde = [plt.Line2D([0], [0], color=color_palette()[0]),
+                       plt.Line2D([0], [0], color=color_palette()[1])]
+        labels_kde = labels_scatter[-2:]
+        ax.legend(handles_scatter[-2:] + handles_kde, labels_scatter[-2:] + labels_kde,
+                  loc='lower center', ncol=2)
+    else:
+        if label == 'age_at_scan' or label == 'bmi':
+            subjects_df[label] = cut(subjects_df[label], bins=3)
+            subjects_df = subjects_df[subjects_df[label] != subjects_df[label].cat.categories[1]]
+        scatterplot(data=subjects_df, x='emb_x', y='emb_y', hue=label, ax=ax, alpha=0.5, size=.3)
+        kdeplot(data=subjects_df, x='emb_x', y='emb_y', hue=label, fill=False, ax=ax)
     if annotate_ids:
         for i, subject_id in enumerate(subjects_df.index):
             ax.annotate(subject_id, (components[i, 0], components[i, 1]), alpha=0.6)
     ax.set_title(f'Latent representations {method.upper()} embeddings')
     ax.axes.xaxis.set_visible(False), ax.axes.yaxis.set_visible(False)
     filename = save_path / f'latents_{method}_{label}.png'
-    plt.savefig(filename)
+    plt.savefig(filename, dpi=150)
     plt.show()
     print(f'Figure saved at {filename}')
 
@@ -158,6 +184,8 @@ if __name__ == '__main__':
                         help='Method to use for manifold learning (PCA, MDS, tSNE, Isomap)')
     parser.add_argument('--label', type=str, default='age_at_scan',
                         help='label used for prediction and plotting latent representations'),
+    parser.add_argument('--color_label', type=str, default=None,
+                        help='label used for coloring the embeddings when doing manifold learning')
     parser.add_argument('--set', type=str, default='val', help='set to evaluate (val or test)')
     parser.add_argument('--balance', action='store_true', help='balance the dataset by age and sex')
     parser.add_argument('--ukbb_size', type=float, default=0.15,
@@ -179,6 +207,8 @@ if __name__ == '__main__':
     embeddings_df = embeddings_df[~embeddings_df[args.label].isna()]
     if args.balance:
         embeddings_df = balance_dataset(embeddings_df, args.label)
+    print(embeddings_df.groupby(args.label)['age_at_scan'].describe())
+    print(embeddings_df.groupby(args.label)['gender'].describe())
 
     data, age_range, bmi_range = load_set(args.dataset, args.set, args.splits_path, args.random_state)
     if args.sample == 0 and not args.manifold:
@@ -196,4 +226,4 @@ if __name__ == '__main__':
             model = load_model(weights_path, device)
             sample(model, dataset, args.age, args.sample, device, save_path)
         elif args.manifold:
-            plot_embeddings(embeddings_df, args.manifold.lower(), args.label, save_path)
+            plot_embeddings(embeddings_df, args.manifold.lower(), args.label, save_path, color_by=args.color_label)
