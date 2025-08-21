7482fe72b3a91dc7a9b8ea4c33debe0087d9b013
Fermin Travi <fermintravi@gmail.com>
2025-08-21 11:24:15 -0300

    Remove unused constants

--- scripts/constants.py
+++ scripts/constants.py
@@ -13,9 +13,3 @@ CFGS_RENAMING = {'age_agnostic': 'Age-agnostic',
                  'bag': 'BAG',
                  'bag_age': 'BAG+Age',
                  'baseline': 'Random'}
-PARAM_GRID = {
-        'learning_rate': [0.001, 0.005, 0.01],
-        'n_layers': [0, 1, 2],
-        'batch_size': [8, 16, 32, 64],
-        'epochs': [3, 5, 10, 15]
-    }
